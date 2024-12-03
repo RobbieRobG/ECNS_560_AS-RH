a53f846853acb49f54f49e54a78e09e02b0549d8
RobbieRobG <179158737+RobbieRobG@users.noreply.github.com>
2024-12-03 15:02:29 -0700

    Update State-County Visualizations.R

--- Code/State-County Visualizations.R
+++ Code/State-County Visualizations.R
@@ -5,6 +5,16 @@ library(tmap)
 library(units)
 
 # load data
-counties = st_read('data/california_counties/CaliforniaCounties.shp')|>
-  st_make_valid()
-"C:\Users\coole\Documents\GitHub\ECNS_560_AS-RH\1. Raw Data\US Shapefiles\cb_2018_us_county_500k\cb_2018_us_county_500k.shp"
+counties = st_read("1. Raw Data/US Shapefiles/cb_2018_us_county_500k/cb_2018_us_county_500k.shp")|>
+  st_make_valid()|>
+  # remove us territories and outlying regions
+  mutate(STATEFP = as.numeric(STATEFP)) |>
+  filter(STATEFP <= 56)|>
+  mutate 
+  
+tmap_mode('view')
+qtm(counties)
+
+
+# load in population data
+popdata = readRDS("2. Merged Data/Intermediate Cleaning Datasets/popdata_00_23_clean.rds")
